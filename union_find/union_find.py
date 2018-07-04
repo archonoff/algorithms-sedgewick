70b5293b46eabf73e040a57850568b4e11dfcdab
oleg-mac <archonoff@gmail.com>
2018-07-04 15:41:08 +0300

    quick union implementation

--- union_find/union_find.py
+++ union_find/union_find.py
@@ -1,24 +1,51 @@
 from sys import stdin
 
 
-class UF:
+class UnionFindBase:
     def __init__(self, n: int):
         self.n = n
         self.array = list(range(n))
 
     def connected(self, p: int, q: int) -> bool:
-        # quick find algorithm
+        raise NotImplementedError
+
+    def union(self, p: int, q: int) -> None:
+        raise NotImplementedError
+
+
+class QuickFind(UnionFindBase):
+    def connected(self, p: int, q: int) -> bool:
+        """Find takes O(1) time"""
         return self.array[p] == self.array[q]
 
     def union(self, p: int, q: int) -> None:
+        """Union takes O(n) time"""
         for i in range(len(self.array)):
             if self.array[i] == self.array[p]:
                 self.array[i] = self.array[q]
 
 
+class QuickUnion(UnionFindBase):
+    """Union works fast because of lazy evaluation"""
+
+    def root(self, i: int):
+        """All algorithm complexity is here"""
+        while i != self.array[i]:
+            i = self.array[i]
+        return i
+
+    def union(self, p: int, q: int) -> None:
+        i = self.root(p)
+        j = self.root(q)
+        self.array[i] = j   # or self.array[j] - must be the same because for root element they should be equal
+
+    def connected(self, p: int, q: int) -> bool:
+        return self.root(p) == self.root(q)
+
+
 def main():
     n = int(input())
-    uf = UF(n)
+    uf = QuickFind(n)
     for line in stdin:
         p, q = [int(s) for s in line.split()]
         if not uf.connected(p, q):
